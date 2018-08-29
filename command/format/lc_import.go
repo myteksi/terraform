81bb9e1207198621c923f8bd791e335a6c5076a2
Lincoln Lee <lincoln.lee@grabtaxi.com>
2018-08-29 13:26:49 +0800

    Get the LC from ASG only
    This will be active and the only one we need
    Avoid giving out multiple LC

--- command/format/lc_import.go
+++ command/format/lc_import.go
@@ -2,62 +2,36 @@ package format
 
 import (
 	"bytes"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
-	"strings"
 )
 
 func lc_import(r *InstanceDiff, cond map[string]string) string {
-
 	name, ok := cond["name_prefix"]
 	if !ok {
 		return ""
 	}
 	names := []*string{}
 	names = append(names, &name)
-	svc := autoscaling.New(session.New())
-	input := &autoscaling.DescribeLaunchConfigurationsInput{
-		LaunchConfigurationNames: []*string{},
+	elbsvc := autoscaling.New(session.New())
+
+	input := &autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{
+			aws.String(name),
+		},
 	}
 
-	list := make([]string, 0)
-	err := svc.DescribeLaunchConfigurationsPages(input, func(result *autoscaling.DescribeLaunchConfigurationsOutput, lastPage bool) bool {
-		for _, res := range result.LaunchConfigurations {
-
-			if strings.HasPrefix(*(res.LaunchConfigurationName), name) {
-				list = append(list, *res.LaunchConfigurationName)
-			}
-
-		}
-		return !lastPage
-	})
 	var buffer bytes.Buffer
 
-	if err != nil {
-		buffer.WriteString("No Import: There is no LC named " + name)
-		buffer.WriteString("\n")
-		return buffer.String()
-		//log.Fatal(err.Error())
-	}
+	result, _ := elbsvc.DescribeAutoScalingGroups(input)
 
-	if len(list) == 0 {
-		buffer.WriteString("No Import: There is no LC with name prefix " + name)
-		buffer.WriteString("\n")
-		return buffer.String()
-	}
-	if len(list) == 1 {
-		buffer.WriteString("terraform import  ")
-		buffer.WriteString(r.Addr.String() + "  ")
-		buffer.WriteString(list[0] + "\n\n")
-		return buffer.String()
-	}
+	LaunchConfigName := *(result.AutoScalingGroups[0].LaunchConfigurationName)
 
-	buffer.WriteString("Multiple LC found\n")
-	for _, res := range list {
-		buffer.WriteString("# terraform import  ")
-		buffer.WriteString(r.Addr.String() + "  ")
-		buffer.WriteString(res + "\n")
-	}
-	buffer.WriteString("\n")
+	buffer.WriteString("terraform import  ")
+	buffer.WriteString(r.Addr.String() + "  ")
+	buffer.WriteString(LaunchConfigName + "\n\n\n")
+
 	return buffer.String()
 }
